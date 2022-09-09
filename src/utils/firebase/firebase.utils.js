9f8485e16382b4a2c070eaeceb9757b3a2a46505
Christian <christian.vismara@gmail.com>
2022-09-09 19:50:11 +0100

    added prettier

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import {
   getAuth,
   signInWithRedirect,
@@ -9,21 +9,19 @@ import {
   signInWithEmailAndPassword,
   signOut,
   onAuthStateChanged,
-} from 'firebase/auth';
-import {
-  getFirestore, doc, getDoc, setDoc,
-} from 'firebase/firestore';
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: 'react-playground-b3226.firebaseapp.com',
-  projectId: 'react-playground-b3226',
-  storageBucket: 'react-playground-b3226.appspot.com',
-  messagingSenderId: '1044574168268',
-  appId: '1:1044574168268:web:1b181b34cc28ee72f48e69',
+  authDomain: "react-playground-b3226.firebaseapp.com",
+  projectId: "react-playground-b3226",
+  storageBucket: "react-playground-b3226.appspot.com",
+  messagingSenderId: "1044574168268",
+  appId: "1:1044574168268:web:1b181b34cc28ee72f48e69",
 };
 
 // Initialize Firebase
@@ -31,22 +29,23 @@ initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 
 provider.setCustomParameters({
-  prompt: 'select_account',
+  prompt: "select_account",
 });
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
-export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, provider);
 
 export const db = getFirestore();
 
 export const getOrCreateUserDocumentFromAuth = async (
   userAuth,
-  additionalInfo,
+  additionalInfo
 ) => {
   if (!userAuth) return null;
 
-  const userDocRef = await doc(db, 'users', userAuth.uid);
+  const userDocRef = await doc(db, "users", userAuth.uid);
 
   let userSnapshot = await getDoc(userDocRef);
 
@@ -85,4 +84,5 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 
 export const signOutUser = () => signOut(auth);
 
-export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);
